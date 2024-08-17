08ad8efcd616d5511a62a5c3c92358aeb9a9568c
Khafid224 <abdulhafid1230@gmail.com>
2024-08-17 22:58:31 +0700

    Membuat Proses Logout

--- src/components/SidebarMenu.vue
+++ src/components/SidebarMenu.vue
@@ -1,5 +1,25 @@
 <script setup>
 
+// import useRouter from vue-router
+import { useRouter } from 'vue-router'
+
+// initialize router
+const router = useRouter()
+
+// import Cookies from js-cookie
+import Cookies from 'js-cookie'
+
+// method to handle logout
+const logout = () => {
+
+    //remove token and user on cookies
+    Cookies.remove('token')
+    Cookies.remove('user')
+
+    // redirect to login
+    router.push({ name: 'login' })
+}
+
 </script>
 
 <template>
@@ -11,7 +31,7 @@
             <div class="list-group">
                 <router-link :to="{ name: 'dashboard' }" class="list-group-item list-group-item-action">Dashboard</router-link>
                 <a href="#" class="list-group-item list-group-item-action">Users</a>
-                <a href="#" class="list-group-item list-group-item-action">Logout</a>
+                <a href="#" @click="logout" class="list-group-item list-group-item-action">Logout</a>
             </div>
         </div>
     </div>
