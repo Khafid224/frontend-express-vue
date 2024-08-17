1df4780c51c38aeede7f8647dd2daaf775f8ce75
Khafid224 <abdulhafid1230@gmail.com>
2024-08-17 23:15:01 +0700

    Menampilkan List Data Users

--- src/views/admin/users/index.vue
+++ src/views/admin/users/index.vue
@@ -3,6 +3,43 @@
 //import sidebar component
 import SidebarMenu from '../../../components/SidebarMenu.vue'
 
+//import ref and onMounted
+import { ref, onMounted } from 'vue';
+
+//import js cookie
+import Cookies from 'js-cookie';
+
+//import api
+import api from '../../../services/api';
+
+//get token from cookies
+const token = Cookies.get('token');
+
+//define state
+const users = ref([]);
+
+//method fetchDataPosts
+const fetchDataUsers = async () => {
+
+    //fetch data 
+    api.defaults.headers.common['Authorization'] = token;
+    await api.get('/api/admin/users')
+
+        .then(response => {
+
+            //set response data to state "users"
+            users.value = response.data.data
+
+        });
+}
+
+//run hook "onMounted"
+onMounted(() => {
+
+    //call method "fetchDataUsers"
+    fetchDataUsers();
+});
+
 </script>
 
 <template>
@@ -19,7 +56,33 @@ import SidebarMenu from '../../../components/SidebarMenu.vue'
                             class="btn btn-sm btn-success rounded shadow-sm border-0">ADD USER</router-link>
                     </div>
                     <div class="card-body">
-                        HALAMAN USERS INDEX
+                        <table class="table table-bordered">
+                            <thead class="bg-dark text-white">
+                                <tr>
+                                    <th scope="col">Full Name</th>
+                                    <th scope="col">Email Address</th>
+                                    <th scope="col" style="width:17%">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-if="users.length == 0">
+                                    <td colspan="4" class="text-center">
+                                        <div class="alert alert-danger mb-0">
+                                            Data Belum Tersedia!
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr v-else v-for="(user, index) in users" :key="index">
+                                    <td>{{ user.name }}</td>
+                                    <td>{{ user.email }}</td>
+                                    <td class="text-center">
+                                        <router-link :to="{ name: 'admin.users.edit', params: { id: user.id } }"
+                                            class="btn btn-sm btn-primary rounded-sm shadow border-0 me-2">EDIT</router-link>
+                                        <button class="btn btn-sm btn-danger rounded-sm shadow border-0">DELETE</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
